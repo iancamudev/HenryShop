4224726ca8578d47bd4c5668e3d5f858f3768528
Franco Aglieri <francoaglieri@hotmail.com>
2022-11-08 18:26:08 -0300

    Post Controller

--- api/src/routes/postRoute.ts
+++ api/src/routes/postRoute.ts
@@ -1,27 +1,24 @@
 import {Router} from "express";
-import { Product } from "../models/Product";
 import { addNewProduct } from "../controllers/PostController";
+
+
 
 require("../mongo")
 const rout = Router();
 
 rout.post("/", async (req, res) => {
     const newProduct = req.body
-    const findProduct = await Product.findOne({name: newProduct.name})
-    if(findProduct){
-      res.status(400).send({error: "Product already exist"})
-    }
-    if(!newProduct){
-      res.status(400).send({error: "Info Missing"})
-    }
     try {
-      if(newProduct){
-        addNewProduct(newProduct);
-        res.status(200).send(newProduct);
-      }
-    } catch (error) {
-      res.status(400).send({message: error})
+      if(!newProduct){
+        res.status(400).send({error: "Info Missing"})
+      }else{
+        await addNewProduct(newProduct);
+        res.status(200).send(newProduct)
     }
+    } catch (err: any) {
+      res.status(400).send({message: err.message})
+    }
+    
 });
 
 export default rout;
