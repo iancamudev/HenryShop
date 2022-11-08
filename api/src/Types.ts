0c9ba0bd5f160f775159200cfa163a18ee3c1d83
Ian Camu <iancamu01@hotmail.com>
2022-11-08 20:13:37 -0300

    Refactorizacion: Se agrego category al modelo, esquema y interface de Products

--- api/src/Types.ts
+++ api/src/Types.ts
@@ -1,12 +1,11 @@
 export interface product {
-    name: String,
-    rating: Number,
-    description: String,
-    price: Number,
-    image: String,
-    stock: Number,
-    colors: Array<string>,
-    sizes: Array<string>
-  }
-  
-  
+  name: String;
+  rating: Number;
+  description: String;
+  price: Number;
+  image: String;
+  stock: Number;
+  category: String;
+  colors: Array<string>;
+  sizes: Array<string>;
+}
--- api/src/models/Product.ts
+++ api/src/models/Product.ts
@@ -1,15 +1,15 @@
 import { Schema, model } from "mongoose";
 
-const productSchema = new Schema({ 
-    name: { type: String, required: true, unique: true },
-    rating: Number,
-    description: { type: String, required: true },
-    price: { type: Number, required: true },
-    image: {type: String, required: true},
-    stock: {type: Number, required: true},
-    colors: Array,
-    sizes: Array
+const productSchema = new Schema({
+  name: { type: String, required: true, unique: true },
+  rating: Number,
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: String, required: true },
+  stock: { type: Number, required: true },
+  category: { type: String, required: true },
+  colors: Array,
+  sizes: Array,
 });
-
 
 export const Product = model("Product", productSchema);
