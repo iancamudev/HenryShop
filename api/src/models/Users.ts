94dc642e486411d08ed417137b6d2a9011adcac8
DeusExMachina203 <ernestodeloayza@gmail.com>
2022-11-09 12:32:46 -0500

    Agregado modelo User

---
+++ api/src/models/Users.ts
@@ -0,0 +1,18 @@
+import {Schema, model} from 'mongoose';
+
+const userSchema = new Schema({
+	name: {type: String, required: true},
+	email: {type: String, required: true},
+	username: {type: String, required: true},
+	password: {type: String, required: true},
+	age: {type: Number, required: true},
+	deleted: {type: Boolean, default: false}
+});
+// modifica el _id de lo que te devuelve la base de datos por id, ademas remueve el __v
+productSchema.set("toJSON", {
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
