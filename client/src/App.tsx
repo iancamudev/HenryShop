d6911b5c63253f9cdfe59aeae0dbbd5186eb453b
Franco Aglieri <francoaglieri@hotmail.com>
2022-11-10 20:10:15 -0300

    Se agrego el ruteo de los componentes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,18 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Login from "./componentes/Login";
 import Register from "./componentes/Register";
+import Home from "./componentes/Home";
 
 function App() {
   return (
     <div className="App flex flex-col items-center bg-gray-100">
-      <Login />
-      <Register />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Login" element={<Login/>}/>
+        <Route path="/Register" element={<Register/>}/>
+        <Route path="/products/:id"/>
+      </Routes>
     </div>
   );
 }
