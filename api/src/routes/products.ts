3b48ba3fac211ba59dd2861907bfdfd19d11c5bd
DeusExMachina203 <ernestodeloayza@gmail.com>
2022-11-10 09:44:53 -0500

    Filtros:

--- api/src/routes/products.ts
+++ api/src/routes/products.ts
@@ -1,13 +1,14 @@
 import { Router, Request, Response } from "express";
 import {
   getAllProductsAdmin,
-  getAllProducts,
-  getAllProductsByCategory,
-  getAllProductsByName,
+  // getAllProducts,
+  // getAllProductsByCategory,
+  // getAllProductsByName,
   addNewProduct,
   getProductById,
   deleteProduct,
   changeProperties,
+  getWithfilters,
 } from "../controllers/product/index";
 require("../mongo");
 
@@ -27,36 +28,51 @@ routes.get("/admin", async (_req: Request, res: Response) => {
 });
 
 
-routes.get("/", async (req: Request, res: Response) => {
-  try {
-    const {category, name} = req.query;
-      const result = await getAllProducts();
+// routes.get("/", async (req: Request, res: Response) => {
+//   try {
+//     const {category, name} = req.query;
+//       const result = await getAllProducts();
       
-      if(category && typeof category === "string" && !name){
-        const resultCategory = await getAllProductsByCategory(category);
-          if(!resultCategory){
-            res.status(200).send({message: "No se encontraron productos en esa categoria"})
-          }
-          else {
-            res.status(200).send(resultCategory);
-          }
+//       if(category && typeof category === "string" && !name){
+//         const resultCategory = await getAllProductsByCategory(category);
+//           if(!resultCategory){
+//             res.status(200).send({message: "No se encontraron productos en esa categoria"})
+//           }
+//           else {
+//             res.status(200).send(resultCategory);
+//           }
           
-      }
-     else if(name && typeof name === "string" && !category){
-        const resultName = await getAllProductsByName(name);
-        if(!resultName){
-          res.status(200).send({message: "No se encontraron productos con ese nombre"})
-        }
-        else {
-          res.status(200).send(resultName);
-        }
-      }
+//       }
+//      else if(name && typeof name === "string" && !category){
+//         const resultName = await getAllProductsByName(name);
+//         if(!resultName){
+//           res.status(200).send({message: "No se encontraron productos con ese nombre"})
+//         }
+//         else {
+//           res.status(200).send(resultName);
+//         }
+//       }
   
-      else {
-        res.status(200).send(result);
-      }
-    }
-  catch (error:any) {
+//       else {
+//         res.status(200).send(result);
+//       }
+//     }
+//   catch (error:any) {
+//     res.status(500).json({error_message:error.message});
+//   }
+// });
+
+routes.get('/', async (req: Request, res:Response) => {
+  try{
+
+    const name = String(req.query.name);
+    const category = String(req.query.category);
+    const order = String(req.query.order);
+    const property = String(req.query.property);
+    const result:any = await getWithfilters(category, name, property, order);
+    if(result)res.status(200).json(result);
+    else res.status(400).json({error_message: "not found"});
+  }catch(error:any){
     res.status(500).json({error_message:error.message});
   }
 });
