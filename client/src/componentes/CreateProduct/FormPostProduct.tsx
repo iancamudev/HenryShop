d6eac8022a5348665124805af42ca01a7104989b
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2022-11-10 20:07:20 -0500

    Update FormPostProduct.tsx

--- client/src/componentes/CreateProduct/FormPostProduct.tsx
+++ client/src/componentes/CreateProduct/FormPostProduct.tsx
@@ -4,7 +4,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup"
 import axios from "axios";
 
-interface formData{
+interface formData {
     name: string;
     rating: number;
     description: string;
@@ -18,7 +18,7 @@ interface formData{
 
 
 const schema = yup.object({
-    name: yup.string().required('debes agregar el nombre del producto'),
+    name: yup.string().required('Debes agregar el nombre del producto'),
     rating: yup.number().min(0).max(5),
     description: yup.string().min(1).max(80).required('Debe haber una descripción, máximo 80 caracteres'),
     price: yup.number().required('no olvides agregar el precio del prodcuto'),
@@ -28,64 +28,85 @@ const schema = yup.object({
     colors: yup.array(),
     sizes: yup.array(),
 })
-.required();
+    .required();
 
-const Form = () =>{
+const Form = () => {
     const { register, handleSubmit, formState: { errors } } = useForm<formData>({
-        resolver : yupResolver(schema)
+        resolver: yupResolver(schema)
     });
-    
+
     const submitForm = handleSubmit((data) => {
         let backData = process.env.REACT_APP_BACKEND_URL;
         if (backData)
-          axios.post(`${backData}/products/`, {}).then((res) => {
-              console.log(res);
+            axios.post(`${backData}/products/`, {}).then((res) => {
+                console.log(res);
             })
-            .catch((err) => console.error(err));
-      });
-    
+                .catch((err) => console.error(err));
+    });
+
     return (
         <div>
-        <form onSubmit={submitForm}>
-                <input {...register('name')} type="text" placeholder="Name..." />
-                {errors?.name && (
-                <p>{errors.name.message}</p>)}
-        
-                <input {...register('rating')} type="text" placeholder="Rating..."/>
-                {errors?.rating && (
-                <p>{errors.rating.message}</p>)}
+            <form onSubmit={submitForm}
+                className="flex justify-center flex-col items-center w-9/12 m-auto"
+            >
+                <div className="mb-3.5 w-full">
+                    <input {...register('name')} type="text" placeholder="Name..." />
+                    {errors?.name && (
+                        <p className="text-red-600 font-bold">{errors.name.message}</p>)}
+                </div>
 
-                <input {...register('description')} type="text" placeholder="Description..."/>
-                {errors?.description && (
-                <p>{errors.description.message}</p>)}
+                <div className="mb-3.5 w-full">
+                    <input {...register('rating')} type="text" placeholder="Rating..." />
+                    {errors?.rating && (
+                        <p className="text-red-600 font-bold">{errors.rating.message}</p>)}
 
-                <input {...register('price')} type="text" placeholder="Price..."/>
-                {errors?.price && (
-                <p>{errors.price.message}</p>)}
+                </div>
 
-                <input {...register('image')} type="text" placeholder="Image..."/>
-                {errors?.image && (
-                <p>{errors.image.message}</p>)}
+                <div className="mb-3.5 w-full">
+                    <input {...register('description')} type="text" placeholder="Description..." />
+                    {errors?.description && (
+                        <p className="text-red-600 font-bold">{errors.description.message}</p>)}
+                </div>
 
-                <input {...register('stock')} type="text" placeholder="Stock..."/>
-                {errors?.stock && (
-                <p>{errors.stock.message}</p>)}
+                <div className="mb-3.5 w-full">
+                    <input {...register('price')} type="text" placeholder="Price..." />
+                    {errors?.price && (
+                        <p className="text-red-600 font-bold">{errors.price.message}</p>)}
+                </div>
 
-                <input {...register('category')} type="text" placeholder="Category..."/>
-                {errors?.category && (
-                <p>{errors.category.message}</p>)}
+                <div className="mb-3.5 w-full">
+                    <input {...register('image')} type="text" placeholder="Image..." />
+                    {errors?.image && (
+                        <p className="text-red-600 font-bold">{errors.image.message}</p>)}
+                </div>
 
-                <input {...register('colors')} type="text" placeholder="Colors..."/>
-                {errors?.colors && (
-                <p>{errors.colors.message}</p>)}
+                <div className="mb-3.5 w-full">
+                    <input {...register('stock')} type="text" placeholder="Stock..." />
+                    {errors?.stock && (
+                        <p className="text-red-600 font-bold">{errors.stock.message}</p>)}
+                </div>
 
-                <input {...register('sizes')} type="text" placeholder="Sizes..."/>
-                {errors?.sizes && (
-                <p>{errors.sizes.message}</p>)}
+                <div className="mb-3.5 w-full">
+                    <input {...register('category')} type="text" placeholder="Category..." />
+                    {errors?.category && (
+                        <p className="text-red-600 font-bold">{errors.category.message}</p>)}
+                </div>
 
-                <button>Agregar producto</button>
+                <div className="mb-3.5 w-full">
+                    <input {...register('colors')} type="text" placeholder="Colors..." />
+                    {errors?.colors && (
+                        <p className="text-red-600 font-bold">{errors.colors.message}</p>)}
+                </div>
 
-        </form>
+                <div className="mb-3.5 w-full">
+                    <input {...register('sizes')} type="text" placeholder="Sizes..." />
+                    {errors?.sizes && (
+                        <p className="text-red-600 font-bold">{errors.sizes.message}</p>)}
+                </div>
+
+                <button className="bg-[#d9d9d9] w-full py-2 rounded-2xl font-bold my-1.5">Agregar producto</button>
+
+            </form>
         </div>
     )
 
