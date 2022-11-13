968f88c31f9f57380ab451a132e66a69e4ce72a7
William Fleitas <69325009+WilliamFleitas@users.noreply.github.com>
2022-11-13 12:38:52 -0300

    conect a cloudinary funcionando con variables de entorno

--- api/utils/cloudinary.ts
+++ api/utils/cloudinary.ts
@@ -1,13 +1,13 @@
 import { v2 as cloudinary } from 'cloudinary'
-
 import dotenv from "dotenv";
-// const { CLOUD_NAME, CLOUD_KEY, CLOUD_SECRET } = process.env;
+dotenv.config();
+
 
 //conect con mi cuenta de cloudinary
 cloudinary.config({ 
-    cloud_name: "dn13gifly", 
-    api_key: "138323822818154", 
-    api_secret: "14dmAK9K42EL1UPdY_wILwCj9XE",
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.CLOUD_KEY, 
+    api_secret: process.env.CLOUD_SECRET,
     secure: true
   });
 //function para subir imagenes a cloudinary
