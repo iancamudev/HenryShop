47ed47f6e868652bd0977c20aa2242e16beb0eb6
William Fleitas <69325009+WilliamFleitas@users.noreply.github.com>
2022-11-13 18:43:32 -0300

    se agrego el archivo cloudinary con una funcion que sube los archivos a la base de datos y que los elimina de la base de datos

--- api/utils/cloudinary.ts
+++ api/utils/cloudinary.ts
@@ -14,3 +14,7 @@ cloudinary.config({
 export async function uploadImage(filePath:any){
  return await  cloudinary.uploader.upload(filePath, { folder: "dbimg"})
 };
+
+export async function deleteImage(public_id: any){
+ return await cloudinary.uploader.destroy(public_id);
+}
