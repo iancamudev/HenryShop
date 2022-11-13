c8b748a6a581be3b8349f3172c14c561e1a0120b
EzeOl123 <107415980+EzeOl123@users.noreply.github.com>
2022-11-13 17:20:56 -0300

    AdminPanel

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,21 +5,23 @@ import Register from "./componentes/Register";
 import CreateProduct from "./componentes/CreateProduct";
 import Home from "./componentes/Home";
 import { DetailProduct } from "./componentes/DetailProduct";
-import  ProductCards  from "./componentes/ProductCards";
+import ProductCards from "./componentes/ProductCards";
+import EditProduct from "./componentes/EditProduct";
+import AdminPanel from "./componentes/Admin/AdminPanel";
 function App() {
   return (
-    
     <div className="App flex flex-col items-center bg-[#FFFDE7]">
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/Login" element={<Login/>}/>
-        <Route path="/Register" element={<Register/>}/>
-        <Route path="/Createproduct" element={<CreateProduct/>}/>
-        <Route path="/products/:id" element={<DetailProduct  />}/>
-        <Route path="/" element={<ProductCards />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Createproduct" element={<CreateProduct />} />
+        <Route path="/products/:id" element={<DetailProduct />} />
+        <Route path="/" element={<ProductCards />} />
+        <Route path="/admin" element={<AdminPanel />} />
+        <Route path="/admin/:id" element={<EditProduct />} />
       </Routes>
     </div>
-    
   );
 }
 
