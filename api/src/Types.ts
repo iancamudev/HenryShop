4484324a4f79f50cbfdb6c3b4745c64b93f8d638
William Fleitas <69325009+WilliamFleitas@users.noreply.github.com>
2022-11-13 02:07:16 -0300

    actualizado el typo de imagen en la interfaz

--- api/src/Types.ts
+++ api/src/Types.ts
@@ -3,7 +3,7 @@ export interface product {
   rating: Number;
   description: String;
   price: Number;
-  image: String;
+  image: object | string;
   stock: Number;
   category: String;
   colors: Array<string>;
