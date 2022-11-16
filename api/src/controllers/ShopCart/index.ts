e05504e6a05de5d4ee31dd85bd414d3af87420fe
DeusExMachina203 <44448300+DeusExMachina203@users.noreply.github.com>
2022-11-15 19:03:09 -0500

    Update index.ts

--- api/src/controllers/ShopCart/index.ts
+++ api/src/controllers/ShopCart/index.ts
@@ -2,7 +2,6 @@ import { Shopping } from "../../models/Shopping";
 
 
  export const addNewShop = async (userId: String, products: Array<object>) =>{
-  console.log("hola aca estoy");
     if(!userId || !products) throw new Error("info missing");
     const newRela = Shopping.create({userId, products});
     return newRela;
