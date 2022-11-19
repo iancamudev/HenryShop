d6538c96a4cd312094212b4d305cfb0f706f0ed0
William Fleitas <69325009+WilliamFleitas@users.noreply.github.com>
2022-11-18 23:02:18 -0300

    limpiando consolelogs

--- api/src/middlewares/userValidation.ts
+++ api/src/middlewares/userValidation.ts
@@ -9,7 +9,10 @@ module.exports = (req: Request, res: any, next: any) => {
   if (authorization && authorization.toLocaleLowerCase().startsWith("bearer")) {
     token = authorization.split(" ")[1]; // obtenemos el token del authorization 'bearer token'
   }
+  
+  
   const decodedToken = jwt.verify(token, process.env.SECRETKEY);
+  
   if (!token || !decodedToken.id) {
     return res.status(401).json({ error: "token missing or invalid" });
   }
