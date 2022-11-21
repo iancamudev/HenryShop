8b9d2f8457e1f37b1a2a21c0b081cd4e948d9e38
DeusExMachina203 <ernestodeloayza@gmail.com>
2022-11-21 10:30:07 -0500

    arreglado estado de redux con usuarios de terceros

--- client/src/redux/slices/UserSlice/UserActions.ts
+++ client/src/redux/slices/UserSlice/UserActions.ts
@@ -3,9 +3,24 @@ import { AppDispatch } from "../../store";
 import { IUserState, setData, clearData } from "./index";
 
 export const setUserData = () => (dispatch: AppDispatch) => {
-  axiosGetCall("/users/getUserByToken").then((response) =>
-    dispatch(setData(response.data))
-  );
+  const session = JSON.parse(window.localStorage.getItem("userSession") as string);
+  if(session.origin === 'default'){
+    axiosGetCall("/users/getUserByToken").then((response) =>{
+      dispatch(setData(response.data));
+    }
+    );
+  }else if(session.origin === 'google'){
+    axiosGetCall("/googleusers/getGoogleUserByToken").then((response) =>{
+      dispatch(setData({username: response.data.name.split(' ')[0], confirmed:response.data.confirmed, token: '' }));
+    }
+    );
+  }else if(session.origin === 'github'){
+    axiosGetCall("/githubusers/getGithubUserByToken").then((response) =>{
+      console.log(response.data);
+      dispatch(setData({username: response.data.username, confirmed:response.data.confirmed, token: '' }));
+    }
+    );
+  }
 };
 
 export const clearUserData = () => (dispatch: AppDispatch) => {
