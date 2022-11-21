451689a4bb2b77737ad6c39874c4113561a6685d
Franco Aglieri <francoaglieri@hotmail.com>
2022-11-21 15:01:19 -0300

    Carrito en la db

--- api/src/routes/Shop.ts
+++ api/src/routes/Shop.ts
@@ -3,18 +3,38 @@ import { addNewShop } from "../controllers/ShopCart";
 import { Product } from "../models/Product";
 import { Shopping } from "../models/Shopping";
 import User from "../models/User";
+const jwt = require("jsonwebtoken");
+import { GoogleUser } from "../models/googleUser";
+import { GithubUser } from "../models/githubUser";
+
 
 require("../mongo")
 const router = Router();
 
-router.post("/:id", async (req: Request, res: Response)=>{
+router.post("/", async (req: Request, res: Response)=>{
     const userId = req.params.id;
     const {products} = req.body;  
-    const usId = await User.findById(userId)
+
+    let token = req.get("authorization");
+    if (token) {
+      token = token.split(" ")[1];
+    }
+    const decodedToken = jwt.verify(token, process.env.SECRETKEY);
+    let user = null;
+    if (decodedToken) {
+      user = await User.findOne({ _id: decodedToken.id });
+    }
+    if(!user){
+      user = await GoogleUser.findOne({ email: decodedToken.email });
+    }
+    if(!user){
+      user = await GithubUser.findOne({ username: decodedToken.username });
+    }
+
     const prodId = await Product.find({_id: products})
     try {
-            if(usId && prodId){
-                const newRela = await addNewShop(usId.id, prodId);
+            if(user && prodId){
+                const newRela = await addNewShop(user.id, prodId);
                 console.log(newRela); 
                 res.status(200).send(newRela);
             }
