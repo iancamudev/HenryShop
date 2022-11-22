642f9684de2b60258da12b5ca8f971c7e7908805
enrique1028 <109766063+enrique1028@users.noreply.github.com>
2022-11-22 15:59:08 -0500

    cambios

--- client/src/componentes/ShoppingCart/CartItem.tsx
+++ client/src/componentes/ShoppingCart/CartItem.tsx
@@ -18,7 +18,7 @@ export function CartItem({id, quantity, color, variante}: CartItemProps) {
     const { increaseCartQuantity, decreaseCartQuantity, removeFromCart } = useShoppingCart()
     const prod = Products.find(i => i.id ===id)
     if(prod === null) return null
-    return (<div className="flex flex-col m-2 p-2 items-center  bg-yellow border rounded-lg shadow-md md:flex-row md:max-w-xl">
+    return (<div className="flex flex-col mx-4 mb-3 p-2 items-center  bg-yellow border rounded-lg shadow-md md:flex-row md:max-w-xl">
             <img src={prod?.image}
             className="object-cover rounded-l-lg w-24 h-70 md:h-auto md:rounded-none md:rounded-l-lg " />
             <div className="flex p-2 w-full justify-between" >
@@ -37,13 +37,13 @@ export function CartItem({id, quantity, color, variante}: CartItemProps) {
                          ${prod?.price as number * quantity}
                     </div></div>
                     
-                    <div className="flex mt-1 p-1 px-4 ml-16 justify-center w-32 border border-gray-600 rounded-xl space-x-3" >
+                    <div className="flex mt-1 p-1 px-4 ml-16 justify-center w-44 border border-gray-600 rounded-xl space-x-10" >
                 <div className="d-flex  justify-center" style={{gap: ".5rem"}}>
-                <Button className="w-9 py-1 px-3 rounded-xl  bg-gray-600 text-white text-xl"  onClick={()=> decreaseCartQuantity(prod?.id as string, color, variante)}>-</Button>
+                <Button className="w-9 py-1 px-3 rounded-xl  bg-gray-500 text-white text-xl"  onClick={()=> decreaseCartQuantity(prod?.id as string, color, variante)}>-</Button>
                     </div>
                     <div  className="fs-3 pt-0.5 font-semibold text-2xl">{quantity}</div> 
                     <div> 
-                <Button className="w-9 py-1 px-3 rounded-xl bg-gray-600 text-white text-xl"  onClick={() => increaseCartQuantity(prod?.id as string, color, variante)}>+</Button>
+                <Button className="w-9 py-1 px-3 rounded-xl bg-gray-500 text-white text-xl"  onClick={() => increaseCartQuantity(prod?.id as string, color, variante)}>+</Button>
                 </div>
                 </div></div>
                 <div className="pt-10 m-0 px-0"><button  style = {{ width: "80px", }} onClick={()=> removeFromCart(prod?.id as string, color, variante)}><DeleteOutlineIcon fontSize="large"/></button>               
