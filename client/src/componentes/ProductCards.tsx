8bbe33c695c6fcffa8249c0d6ec76f5cc996c455
SebasUNLu <SebasUNLu@gmail.com>
2022-11-22 17:16:13 -0300

    refactor(productList): acciones redux, se vacia la lista, se deja un estado de error, y agrego loading lazy a imagenes

--- client/src/componentes/ProductCards.tsx
+++ client/src/componentes/ProductCards.tsx
@@ -1,53 +1,40 @@
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../hooks";
-import { setLoading } from "../redux/slices/ProductSlice";
 import { getAllProducts } from "../redux/slices/ProductSlice/productActions";
 import ProductCard from "./ProductCard";
 import { Loading } from "./Loading";
 // const gifLoading = require("../assets/gifLoading.gif");
 
 const ProductCards = () => {
-  const Products = useAppSelector((state) => state.products.productList);
-  const filters = useAppSelector((state) => state.filterState.filters);
-  const loading = useAppSelector((state) => state.products.loading );
-  
+  const { filters } = useAppSelector((state) => state.filterState);
+  const { productList, loading, error } = useAppSelector(
+    (state) => state.products
+  );
+
   const dispatch = useAppDispatch();
   useEffect(() => {
     dispatch(getAllProducts(null, filters));
-    
   }, [dispatch, filters]);
-   
- if(loading){
-  
-  return (
-    <div>
-      <Loading/>
-    </div>
-  )
- }
-   
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) return <h1>{error}</h1>;
+
   return (
     <div className="flex flex-col items-center">
-      <div className="mb-4">
-        <h3 className="p-4">Nuestros Productos🚀</h3>
-        <div className="bg-yellow h-3 negative"></div>
-      </div>
-
-        {
-         Products.length ?
-              Products.map((producto, index) => {
-              return <ProductCard key={index} product={producto} />;})
-              :
-              <div>
-              <p>No se encontraron productos</p>
-              
-              </div>
-             
-        }
-        
+      {productList.length ? (
+        productList.map((producto, index) => {
+          return <ProductCard key={index} product={producto} />;
+        })
+      ) : (
+        <div>
+          <p>No se encontraron productos</p>
+        </div>
+      )}
     </div>
   );
-   
 };
 
 export default ProductCards;
