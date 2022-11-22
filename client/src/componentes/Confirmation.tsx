e33580d4f9e03b20fa0878ea3e92282b2d02f4f0
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2022-11-22 15:08:45 -0500

    Update Confirmation.tsx

--- client/src/componentes/Confirmation.tsx
+++ client/src/componentes/Confirmation.tsx
@@ -1,24 +1,24 @@
 import axios from "axios";
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 import { BsFillCheckSquareFill } from "react-icons/bs";
 import { useNavigate, useParams } from "react-router-dom";
 
 const Confirmation = () => {
-    let backData = process.env.REACT_APP_BACKEND_URL;
-    const navigate = useNavigate()
-    const RouteToHome = () => {
-        let path = '/'
-        navigate(path)
-    }
-    const {token} = useParams();
+  let backData = process.env.REACT_APP_BACKEND_URL;
+  const navigate = useNavigate()
+  const RouteToHome = () => {
+    let path = '/'
+    navigate(path)
+  }
+  const { token } = useParams();
 
-    useEffect(()=>{
-        axios.get(`${backData}/users/confirmation/${token}`)
-    }, [])
+  useEffect(() => {
+    axios.get(`${backData}/users/confirmation/${token}`)
+  }, [])
   return (
     <div className="flex flex-col mt-5 items-center">
       <h1 >Email Confirmado</h1>
-      <BsFillCheckSquareFill onClick={RouteToHome}className="h-10 w-10 text-green-600 hover:h-12 hover:w-12 hover:text-green-400 hover:duration-400 duration-300" />
+      <BsFillCheckSquareFill onClick={RouteToHome} className="h-10 w-10 text-green-600 hover:h-12 hover:w-12 hover:text-green-400 hover:duration-400 duration-300" />
     </div>
   );
 };
