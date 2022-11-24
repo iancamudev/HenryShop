d2e7a86d4dcc507ae7539c0233eac9974a91775a
SebasUNLu <SebasUNLu@gmail.com>
2022-11-24 17:24:34 -0300

    Soluciono el modelo de Review (de mala manera)

--- api/src/controllers/review/index.ts
+++ api/src/controllers/review/index.ts
@@ -1,17 +1,27 @@
 import Review from "../../models/Review";
+import Product from "../../models/Product";
+import User from "../../models/User";
 import { product, review, user } from "../../Types";
 
-export const addNewReview = async (
-  text: string, rating: number, userId: object, productId: object
-) => {
+export const addNewReview = async (text: string, rating: number, userId: object, productId: object) => {
+
   if (
     !text ||
     !rating ||
     !productId ||
     !userId
   ) throw new Error("Flata enviar datos");
-
-  const newReview = await Review.create({ text, rating, userId, productId })
+  const userReview = {
+    text,
+    rating,
+    productId
+  }
+  const productReview = {
+    text,
+    rating,
+    userId
+  }
+  await User.findByIdAndUpdate(userId, { '$push': { 'reviews': userReview } })
+  await Product.findByIdAndUpdate(productId, { '$push': { 'reviews': productReview } })
   console.log('created')
-  return newReview;
 }
--- api/src/routes/reviews.ts
+++ api/src/routes/reviews.ts
@@ -14,9 +14,13 @@ const router = Router();
 router.post('/', userValidation, async (req: Request, res: Response) => {
   const { userId, productId, text, rating } = req.body;
   try {
+    // añadir la review tanto en el user como en el product
     const user = await User.findById(userId);
+    // validaciones de si esta en default, google o github
     const product = await Product.findById(productId);
 
+    if(!product && !user) throw new Error('Producto o usuario invalido')
+    
     const review = await addNewReview(text, rating, user._id, product._id) 
 
     // await User.findByIdAndUpdate(user._id, { reviews: [...user.reviews, { info: review._id }] })
