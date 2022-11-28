971c4baf09ea25771cf62f71e86fca82872bcdf4
enrique1028 <109766063+enrique1028@users.noreply.github.com>
2022-11-28 12:16:32 -0500

    succes, failure

--- client/src/componentes/Success.tsx
+++ client/src/componentes/Success.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import axios from "axios";
-import { ImHome3 } from "react-icons/im";
+import { ImHome3, ImCart } from "react-icons/im";
 import { useShoppingCart } from "./ShoppingCart/ContextShoppingCart";
 import { Link } from "react-router-dom";
 import Header from "./Header";
@@ -38,17 +38,23 @@ const Success = () => {
   },[])
   return (
     <div className="w-full flex flex-col items-center h-screen">
-      <Header />
-
-      <div className="mt-32 flex flex-col items-center gap-4">
+        <div className="mt-32 flex flex-col items-center gap-4">
         <img src={henryFull} className="w-64" alt="logoHenry" />
         <h5>Gracias por tu compra!</h5>
-        <Link to="/">
+        <div className="flex">
+          <Link to="/" className="px-3">
           <div className="w-fit h-10 bg-yellow flex flex-row gap-4 p-2 items-center rounded-md hover:bg-gray-100 hover:duration-300 duration-300 cursor-pointer border-b-2 border-solid border-black">
             <ImHome3 className="w-6 h-6" />
             <p>Volver al inicio</p>
           </div>
         </Link>
+        <Link to="/shopping" className="px-3">
+          <div className="w-fit h-10 bg-yellow flex flex-row gap-4 p-2 items-center rounded-md hover:bg-gray-100 hover:duration-300 duration-300 cursor-pointer border-b-2 border-solid border-black">
+            <ImCart className="w-6 h-6" />
+            <p>Ver mi compra</p>
+          </div>
+        </Link>
+        </div>        
       </div>
     </div>
   );
