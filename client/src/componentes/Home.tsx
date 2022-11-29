2a69f2afd9741ebf0b39e867d1ba0149736c8cc9
enrique1028 <109766063+enrique1028@users.noreply.github.com>
2022-11-29 02:14:20 -0500

    footer

--- client/src/componentes/Home.tsx
+++ client/src/componentes/Home.tsx
@@ -4,6 +4,7 @@ import Header from "./Header";
 import Filters from "./Filters";
 import ProductList from "./Products/ProductList";
 import Carrousel from "./carrousel/Carrousel";
+import Footer from "./Footer";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Filters />
       <ProductList />
       <Pagination />
+      <Footer />
     </>
   );
 };
