fb08def78b6677a921a6c10d0a9e3a9bdbf1c2f3
EzeOl123 <ezeolivera002@gmail.com>
2022-11-29 09:50:49 -0300

    AddHomeANDIconsMenuPanelAdmin

--- client/src/componentes/Admin/MenuPanel.tsx
+++ client/src/componentes/Admin/MenuPanel.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { resourceLimits } from 'worker_threads';
 import { BiUser } from 'react-icons/bi'
+import {AiFillHome, AiOutlineShopping} from 'react-icons/ai'
+import {FaRegMoneyBillAlt} from 'react-icons/fa'
 import { array } from 'yup';
 const MenuPanel = () => {
     const REACT_APP_BACKEND_URL: string = process.env
@@ -52,23 +54,39 @@ const MenuPanel = () => {
 //   }, [currentSide]);
 //   console.log(arraylado[currentSide])
     return (
-        <div className='invisible xl:visible flex flex-col h-screen bg-slate-900 xl:pt-4 xl:px-10 flex-start justify-start xl:items-center text-white text-bold p-0 h-0 w-0 xl:h-screen xl:w-auto'>
-            <div className={`relative flex flex-col items-center xl:px-4 xl:pt-4 xl:rounded-full xl:border-2 xl:border-yellow xl:border-opacity-80 xl:shadow-xl`} >
-              <BiUser className='xl:h-12 xl:w-12'/>
-              <h5 className='mb-4'>{userProps}</h5>
-            </div>
-            <h4>Panel</h4>
-            <div className='border-t-2 border-solid border-gray-200 xl:pt-2 xl:px-5'>
+        <div className='invisible xl:visible flex flex-col h-screen bg-slate-900 flex-start justify-start xl:items-center text-white text-bold p-0 h-0 w-0 xl:h-screen xl:w-auto'>
+               <div className={`relative flex flex-col items-center xl:px-4 xl:pt-4 xl:rounded-full xl:border-2 xl:border-yellow xl:border-opacity-80 xl:mt-4 xl:shadow-xl`} >
+                 <BiUser className='xl:h-12 xl:w-12'/>
+                 <h5 className='mb-4'>{userProps}</h5>
+               </div>
+            <h4 className=' border-b-2 mt-4 xl:pb-2 border-solid border-white xl:px-16 '>Panel</h4>
+              <div className=' xl:pt-2 xl:mt-2 xl:px-5'>
                 <Link to='/admin'>
-                <h5  className= {path == "admin" ? 'border-b-2 border-solid border-gray-400 hover:cursor-pointer hover:border-b-2 hover:border-gray-400 hover:duration-400 duration-300' : 'border-b-2 border-solid border-slate-900 hover:cursor-pointer hover:border-b-2 hover:border-gray-400 hover:duration-400 duration-300'}>Products</h5>
+                    <div className='flex flex-row items-center gap-1'>
+                    <AiOutlineShopping className='h-5 w-5 mb-1'/>
+                    <h5  className= {path == "admin" ? 'border-b-2 border-solid border-gray-400 hover:cursor-pointer hover:border-b-2 hover:border-gray-400 hover:duration-400 duration-300' : 'border-b-2 border-solid border-slate-900 hover:cursor-pointer hover:border-b-2 hover:border-gray-400 hover:duration-400 duration-300'}>Products</h5>
+                    </div>
                 </Link>
                 <Link to='/admin/users'>
-                <h5 className={path == "users" ? 'border-b-2 border-solid border-gray-400 hover:cursor-pointer hover:border-b-2 hover:border-gray-400 hover:duration-400 duration-300' : 'border-b-2 border-solid border-slate-900 hover:cursor-pointer hover:border-b-2 hover:border-gray-400 hover:duration-400 duration-300'}>Users</h5>
+                  <div className='flex flex-row items-center gap-1'>
+                       <BiUser className='h-5 w-5 mb-1'/>
+                       <h5 className={path == "users" ? 'border-b-2 border-solid border-gray-400 hover:cursor-pointer hover:border-b-2 hover:border-gray-400 hover:duration-400 duration-300' : 'border-b-2 border-solid border-slate-900 hover:cursor-pointer hover:border-b-2 hover:border-gray-400 hover:duration-400 duration-300'}>Users</h5>
+                  </div>
                 </Link>
+
                 <Link to='/admin/payments'>
-                <h5 className={path == "payments" ? 'border-b-2 border-solid border-gray-400 hover:cursor-pointer hover:border-b-2 hover:border-gray-400 hover:duration-400 duration-300' : 'border-b-2 border-solid border-slate-900 hover:cursor-pointer hover:border-b-2 hover:border-gray-400 hover:duration-400 duration-300'}>Payments</h5>
+                   <div className='flex flex-row items-center gap-1'>
+                    <FaRegMoneyBillAlt className='h-5 w-5 '/>
+                    <h5 className={path == "payments" ? 'border-b-2 border-solid border-gray-400 hover:cursor-pointer hover:border-b-2 hover:border-gray-400 hover:duration-400 duration-300' : 'border-b-2 border-solid border-slate-900 hover:cursor-pointer hover:border-b-2 hover:border-gray-400 hover:duration-400 duration-300'}>Payments</h5>
+                    </div>
                 </Link>
+                   <div className='flex flex-row gap-1 mt-2 items-center'>
+                    <AiFillHome className='h-6 w-6 mb-1'/>
+                    <Link to='/'><h5 className='hover:cursor-pointer hover:border-b-2 hover:border-gray-400 hover:duration-400 duration-300'>Home</h5></Link>
+                    </div>
             </div>
+            {/* <h4 className=' border-t-2 mt-4 xl:pb-2 border-solid border-white xl:px-16 '>Rutas</h4> */}
+   
         </div>
     );
 };
