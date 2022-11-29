c8dbc8df250fe7e7ffabcb13523ab51d2ea93f1d
SebasUNLu <SebasUNLu@gmail.com>
2022-11-29 01:24:09 -0300

    Estilos ld

--- client/src/componentes/User/UserInfoShow.tsx
+++ client/src/componentes/User/UserInfoShow.tsx
@@ -18,9 +18,9 @@ interface IProps {
 const UserInfoShow = ({ user }: IProps) => {
   const { name, username, email, birthday, confirmed } = user;
   return (
-    <div className="m-auto my-8 w-full">
-      <div className='md:flex md:justify-evenly'>
-        <div className="md:w-7/12 md:flex md:flex-col md:items-center">
+    <div className="m-auto my-8 w-full lg:text-xl">
+      <div className='md:flex md:justify-evenly lg:justify-center'>
+        <div className="md:w-7/12 md:flex md:flex-col md:items-center lg:w-5/12">
           <h1 className="text-3xl">Perfíl de {name}</h1>
           <hr className='md:w-8/12' />
           <UserFields name="Nombre" value={name} />
@@ -28,7 +28,7 @@ const UserInfoShow = ({ user }: IProps) => {
           <UserFields name="E-Mail" value={email} />
           <UserFields name="Cumpleaños" value={birthday} />
         </div>
-        <div className="flex flex-col items-center md:w-2/12">
+        <div className="flex flex-col items-center md:w-2/12 lg:justify-center">
           <Link to="/shopping">
             <button
               className={`bg-yellow p-2 rounded-sm font-bold border-b-2 border-black`}
@@ -49,7 +49,7 @@ const UserInfoShow = ({ user }: IProps) => {
               disabled={!confirmed}
               className={`${
                 confirmed ? "bg-yellow" : "bg-slate-400 line-through"
-              } p-2 rounded-sm font-bold border-b-2 border-black mt-4`}
+              } p-2 rounded-sm font-bold border-b-2 border-black mt-4 lg:mt-8`}
             >
               Editar Información
             </button>
